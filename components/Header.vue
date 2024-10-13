deed3b72d7185946ebe1cb955ca9ff01244aa8c0
YounesDjelloul <younesdjelloul14@gmail.com>
2024-10-14 03:33:38 +0800

    header component

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header-container">
+    hey header
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 
+import Header from "~/components/Header.vue";
 </script>
 
 <template>
   <div class="dashboard-container">
-    Hey Dashboard
+    <Header />
   </div>
 </template>
 
 <style scoped lang="scss">
-
+  .dashboard-container {
+    width: 100%;
+    height: 100vh;
+    background-color: #EFEFEF;
+  }
 </style>
