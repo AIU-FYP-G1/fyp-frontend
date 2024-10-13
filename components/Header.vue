332f600dd31cd11823835bd85f9c6e1f7cbe84df
YounesDjelloul <younesdjelloul14@gmail.com>
2024-10-14 03:55:57 +0800

    dashboard navigation

--- components/Header.vue
+++ components/Header.vue
@@ -4,10 +4,62 @@
 
 <template>
   <div class="header-container">
-    hey header
+    <div class="aiu-logo-container">
+      <img src="@/public/images/aiu-logo.svg" alt="AIU SCI Logo" class="logo">
+    </div>
+    <div class="navigation-container">
+      <div><NuxtLink to='/dashboard'>home</NuxtLink></div>
+      <div><NuxtLink to='/about'>about</NuxtLink></div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.header-container {
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
+  .aiu-logo-container {
+    width: 50px;
+    height: 50px;
+    margin-right: 50px;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .navigation-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 110px;
+
+    > div {
+      cursor: pointer;
+      text-transform: uppercase;
+      font-weight: 700;
+      font-size: 14px;
+
+      a {
+        text-decoration: none;
+        color: #464646;
+
+        &:hover {
+          color: #050339;
+        }
+
+        &.router-link-active {
+          color: #4A6AF5;
+        }
+      }
+    }
+  }
+}
 </style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -14,5 +14,6 @@ import Header from "~/components/Header.vue";
     width: 100%;
     height: 100vh;
     background-color: #EFEFEF;
+    padding: 25px 40px;
   }
 </style>
