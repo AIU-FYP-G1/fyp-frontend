35a46d42c448ba4b545dbf7aad576f89ba950afa
YounesDjelloul <younesdjelloul14@gmail.com>
2024-11-16 03:32:56 +0800

    Header in the about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,42 +3,61 @@
 </script>
 
 <template>
-  <div class="about-container">
-    <div class="title">About the project</div>
-    <hr>
-    <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid, cumque dolor dolore doloremque eligendi excepturi iste, laudantium libero modi necessitatibus rerum sequi soluta tenetur vitae. Id itaque libero voluptatum.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam asperiores dolorum eligendi, id illo impedit ipsam mollitia nemo quis sequi tenetur. Dicta doloremque mollitia nobis, odit perferendis repudiandae voluptatum!
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+  <div class="about">
+    <Header/>
+    <div class="about-container">
+      <div class="title">About the project</div>
+      <hr>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid, cumque dolor dolore doloremque eligendi
+        excepturi iste, laudantium libero modi necessitatibus rerum sequi soluta tenetur vitae. Id itaque libero
+        voluptatum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam asperiores dolorum eligendi, id
+        illo impedit ipsam mollitia nemo quis sequi tenetur. Dicta doloremque mollitia nobis, odit perferendis
+        repudiandae voluptatum!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error laboriosam nesciunt pariatur provident. Aliquam
+        dolores doloribus harum nobis quisquam reiciendis veritatis vero. Alias atque, cum dolorum eum minus vel vitae?
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .about-container {
-    padding: 70px 210px 0 130px;
+.about {
+  background-color: #EFEFEF;
+  padding: 25px 40px;
+  height: 100vh;
+}
 
-    .title {
-      font-size: 25px;
-      font-family: 'Jost', sans-serif;
-      color: #242424;
-      font-weight: 700;
-      width: 100%;
-    }
+.about-container {
+  padding: 70px 150px 0 110px;
 
-    hr {
-      color: #000;
-    }
-
-    .content {
-      margin-top: 40px;
-      width: 100%;
-      color: #464646;
-    }
+  .title {
+    font-size: 25px;
+    font-family: 'Jost', sans-serif;
+    color: #242424;
+    font-weight: 700;
+    width: 100%;
   }
+
+  hr {
+    color: #000;
+  }
+
+  .content {
+    margin-top: 40px;
+    width: 100%;
+    color: #464646;
+  }
+}
 </style>
