7808d2ae4784afa9efe4f9b2d671c9f867e13e2c
YounesDjelloul <younesdjelloul14@gmail.com>
2024-11-16 16:31:33 +0800

    extract and move security settings

---
+++ components/SecuritySettingsForm.vue
@@ -0,0 +1,182 @@
+<script setup lang="ts">
+
+import Loader from "~/components/base/Loader.vue";
+import {ref} from "vue";
+import {toast} from "vue3-toastify";
+import {z} from "zod";
+import {useAuth} from "~/stores/auth";
+
+const auth = useAuth()
+
+
+const securitySchema = z.object({
+  old_password: z
+      .string()
+      .min(8, {message: "Password must be at least 8 characters long"})
+      .max(100, {message: "Password cannot be longer than 100 characters"}),
+  password1: z
+      .string()
+      .min(8, {message: "Password must be at least 8 characters long"})
+      .max(100, {message: "Password cannot be longer than 100 characters"}),
+  password2: z
+      .string()
+      .min(8, {message: "Password must be at least 8 characters long"})
+      .max(100, {message: "Password cannot be longer than 100 characters"}),
+}).refine((data) => data.password1 === data.password2, {
+  message: "Passwords must match",
+  path: ["password2"],
+});
+
+let securityErrors = reactive({
+  old_password: '',
+  password1: '',
+  password2: '',
+});
+
+let securityState = reactive({
+  old_password: '',
+  password1: '',
+  password2: '',
+  new_password: '',
+});
+
+watchEffect(() => {
+  const result = securitySchema.safeParse(securityState);
+  console.log(result);
+  if (!result.success) {
+    result.error.errors.forEach((err) => {
+      securityErrors[err.path[0]] = err.message;
+    });
+  } else {
+    securityErrors = {old_password: '', password1: '', password2: ''};
+    securityErrors.old_password = ''
+  }
+});
+
+const isSubmitting = ref(false)
+
+const clearSecurityForm = () => {
+  securityState.old_password = '';
+  securityState.password1 = '';
+  securityState.password2 = '';
+}
+
+const updateProfileSecurity = async () => {
+  try {
+    isSubmitting.value = true
+
+    securityErrors.old_password = '';
+    securityErrors.password1 = '';
+    securityErrors.password2 = '';
+
+    const result = securitySchema.safeParse(securityState);
+    if (!result.success) {
+      result.error.errors.forEach((err) => {
+        securityErrors[err.path[0]] = err.message;
+      });
+      return;
+    }
+
+    let payload = {
+      old_password: securityState.old_password,
+      new_password: securityState.password1,
+    }
+
+    await auth.changeProfilePassword(payload)
+
+    setTimeout(() => {
+      toast.success("Password Updated successfully", {
+        bodyClassName: 'toast-body'
+      });
+      clearSecurityForm()
+    }, 600)
+  } catch (error: Error) {
+    const {response: {data}} = error
+
+    for (const [path, messages] of Object.entries(data)) {
+      securityErrors[path] = messages[0]
+    }
+
+    toast.error("An error occurred.", {
+      bodyClassName: 'toast-body'
+    });
+  } finally {
+    setTimeout(() => {
+      isSubmitting.value = false
+    }, 600)
+  }
+}
+</script>
+
+<template>
+  <div class="security-settings-wrapper">
+    <div class="tab-content-header">
+      <div class="text">
+        Account Security
+        <div>Edit your account security details</div>
+      </div>
+      <div class="navigation">
+        <button @click="navigateTo('/dashboard')">
+          <UIcon name="cil:arrow-left"/>
+          Go Back
+        </button>
+        <button @click="updateProfileSecurity">Save Changes</button>
+      </div>
+    </div>
+    <div class="general-container">
+      <Loader v-if="isSubmitting"/>
+      <UForm v-else :schema="securitySchema" :state="securityState">
+        <div class="other-info group">
+          <div class="title">
+            Change Password
+            <div>For an improved account security</div>
+          </div>
+          <div class="input-container">
+            <label for="old_password">Old Password</label>
+            <input
+                v-model="securityState.old_password"
+                type="password"
+                id="old_password"
+                placeholder="Old Password"
+            />
+            <transition name="scale-fade">
+              <div v-if="securityErrors.old_password" class="error">{{ securityErrors.old_password }}</div>
+            </transition>
+          </div>
+          <div class="input-container">
+            <label for="password1">New Password</label>
+            <input
+                v-model="securityState.password1"
+                type="password"
+                id="password1"
+                placeholder="New Password"
+            />
+            <transition name="scale-fade">
+              <div v-if="securityErrors.password1" class="error">{{ securityErrors.password1 }}</div>
+            </transition>
+          </div>
+          <div class="input-container">
+            <label for="password2">Repeat New Password</label>
+            <input
+                v-model="securityState.password2"
+                type="password"
+                id="password2"
+                placeholder="Repeat New Password"
+            />
+            <transition name="scale-fade">
+              <div v-if="securityErrors.password2" class="error">{{ securityErrors.password2 }}</div>
+            </transition>
+          </div>
+        </div>
+      </UForm>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "assets/scss/settings.scss";
+
+.security-settings-wrapper {
+  width: 100%;
+}
+</style>
--- pages/settings.vue
+++ pages/settings.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import {z} from "zod";
 import {ref} from "vue";
 import {toast} from "vue3-toastify";
 import {useAuth} from "~/stores/auth";
@@ -8,9 +7,6 @@ import Loader from '~/components/base/Loader.vue'
 definePageMeta({
   middleware: 'auth'
 })
-
-let {$axios} = useNuxtApp()
-const api = $axios()
 
 const auth = useAuth()
 
@@ -30,108 +26,8 @@ onMounted(async () => {
   }
 })
 
-const securitySchema = z.object({
-  old_password: z
-      .string()
-      .min(8, {message: "Password must be at least 8 characters long"})
-      .max(100, {message: "Password cannot be longer than 100 characters"}),
-  password1: z
-      .string()
-      .min(8, {message: "Password must be at least 8 characters long"})
-      .max(100, {message: "Password cannot be longer than 100 characters"}),
-  password2: z
-      .string()
-      .min(8, {message: "Password must be at least 8 characters long"})
-      .max(100, {message: "Password cannot be longer than 100 characters"}),
-}).refine((data) => data.password1 === data.password2, {
-  message: "Passwords must match",
-  path: ["password2"],
-});
-
-let securityErrors = reactive({
-  old_password: '',
-  password1: '',
-  password2: '',
-});
-
-let securityState = reactive({
-  old_password: '',
-  password1: '',
-  password2: '',
-  new_password: '',
-});
-
 const activeTab = ref('general')
 
-watchEffect(() => {
-  if (activeTab.value === 'security') {
-    const result = securitySchema.safeParse(securityState);
-    console.log(result);
-    if (!result.success) {
-      result.error.errors.forEach((err) => {
-        securityErrors[err.path[0]] = err.message;
-      });
-    } else {
-      securityErrors = {old_password: '', password1: '', password2: ''};
-      securityErrors.old_password = ''
-    }
-  }
-
-});
-
-const isSubmitting = ref(false)
-
-const clearSecurityForm = () => {
-  securityState.old_password = '';
-  securityState.password1 = '';
-  securityState.password2 = '';
-}
-
-const updateProfileSecurity = async () => {
-  try {
-    isSubmitting.value = true
-
-    securityErrors.old_password = '';
-    securityErrors.password1 = '';
-    securityErrors.password2 = '';
-
-    const result = securitySchema.safeParse(securityState);
-    if (!result.success) {
-      result.error.errors.forEach((err) => {
-        securityErrors[err.path[0]] = err.message;
-      });
-      return;
-    }
-
-    let payload = {
-      old_password: securityState.old_password,
-      new_password: securityState.password1,
-    }
-
-    await auth.changeProfilePassword(payload)
-
-    setTimeout(() => {
-      toast.success("Password Updated successfully", {
-        bodyClassName: 'toast-body'
-      });
-      clearSecurityForm()
-    }, 600)
-  } catch (error: Error) {
-    const {response: {data}} = error
-
-    for (const [path, messages] of Object.entries(data)) {
-      securityErrors[path] = messages[0]
-    }
-
-    toast.error("An error occurred.", {
-      bodyClassName: 'toast-body'
-    });
-  } finally {
-    setTimeout(() => {
-      isSubmitting.value = false
-    }, 600)
-  }
-}
 </script>
 
 <template>
@@ -173,66 +69,7 @@ const updateProfileSecurity = async () => {
           <GeneralSettingsForm :original-profile="originalProfile"/>
         </div>
         <div class="tab-content" v-else-if="activeTab === 'security'">
-          <div class="tab-content-header">
-            <div class="text">
-              Account Security
-              <div>Edit your account security details</div>
-            </div>
-            <div class="navigation">
-              <button @click="navigateTo('/dashboard')">
-                <UIcon name="cil:arrow-left"/>
-                Go Back
-              </button>
-              <button @click="updateProfileSecurity">Save Changes</button>
-            </div>
-          </div>
-          <div class="general-container">
-            <Loader v-if="isSubmitting"/>
-            <UForm v-else :schema="securitySchema" :state="securityState">
-              <div class="other-info group">
-                <div class="title">
-                  Change Password
-                  <div>For an improved account security</div>
-                </div>
-                <div class="input-container">
-                  <label for="old_password">Old Password</label>
-                  <input
-                      v-model="securityState.old_password"
-                      type="password"
-                      id="old_password"
-                      placeholder="Old Password"
-                  />
-                  <transition name="scale-fade">
-                    <div v-if="securityErrors.old_password" class="error">{{ securityErrors.old_password }}</div>
-                  </transition>
-                </div>
-                <div class="input-container">
-                  <label for="password1">New Password</label>
-                  <input
-                      v-model="securityState.password1"
-                      type="password"
-                      id="password1"
-                      placeholder="New Password"
-                  />
-                  <transition name="scale-fade">
-                    <div v-if="securityErrors.password1" class="error">{{ securityErrors.password1 }}</div>
-                  </transition>
-                </div>
-                <div class="input-container">
-                  <label for="password2">Repeat New Password</label>
-                  <input
-                      v-model="securityState.password2"
-                      type="password"
-                      id="password2"
-                      placeholder="Repeat New Password"
-                  />
-                  <transition name="scale-fade">
-                    <div v-if="securityErrors.password2" class="error">{{ securityErrors.password2 }}</div>
-                  </transition>
-                </div>
-              </div>
-            </UForm>
-          </div>
+          <SecuritySettingsForm/>
         </div>
       </div>
     </div>
@@ -342,145 +179,6 @@ const updateProfileSecurity = async () => {
       border: 1px solid #cccccc;
       border-radius: 7px;
       padding-bottom: 40px;
-
-      .tab-content-header {
-        width: 100%;
-        height: fit-content;
-        padding: 13px 15px 13px 20px;
-        margin-bottom: 30px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-bottom: 1px solid #ccc;
-
-        .text {
-          font-size: 15px;
-
-          div {
-            font-size: 12px;
-            color: #7d7d7d;
-          }
-        }
-
-        button {
-          font-size: 14px;
-          margin-right: 10px;
-          border-radius: 3px;
-          padding: 5px 13px;
-          background-color: rgba(201, 201, 201, 0.2);
-          display: inline-flex;
-          align-items: center;
-
-          span {
-            margin-right: 10px;
-          }
-
-          &:last-child {
-            margin-right: 0;
-            background-color: #5F94F5;
-            color: #fff;
-            font-weight: 500;
-            padding: 5px 15px;
-          }
-        }
-      }
-
-      .general-container {
-        width: 75%;
-        margin-left: 40px;
-
-        .group {
-          margin-bottom: 40px;
-
-          .title {
-            font-size: 16.5px;
-            color: #242424;
-            font-weight: 600;
-            margin-bottom: 35px;
-
-            div {
-              font-size: 13px;
-              color: #5D5D5D;
-              font-weight: normal;
-            }
-          }
-
-          &:last-child {
-            margin-bottom: 0;
-          }
-        }
-
-        .update-avatar {
-          .upload-container {
-            position: relative;
-            margin: 0 auto;
-            width: fit-content;
-
-            img {
-              border-radius: 50%;
-              width: 75px;
-              height: 75px;
-              cursor: pointer;
-              outline: 3.2px solid #FFF;
-            }
-
-            .edit-button {
-              position: absolute;
-              bottom: 3px;
-              right: -3px;
-              background-color: #5F94F5;
-              padding: 8px 3px;
-              border-radius: 50%;
-              width: 25px;
-              height: 25px;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              cursor: pointer;
-              outline: 1.2px solid #FFF;
-
-              span {
-                color: white;
-                font-size: 13.4px;
-              }
-            }
-          }
-        }
-
-        .other-info {
-
-          .input-container {
-            margin-bottom: 1rem;
-            display: flex;
-            flex-direction: column;
-
-            &:last-child {
-              margin-bottom: 0;
-            }
-
-            label {
-              font-size: 12.5px;
-              color: #5D5D5D;
-              margin-bottom: 8px;
-              font-family: 'Inter', sans-serif;
-              font-weight: 500;
-            }
-
-            input {
-              color: #464646 !important;
-              border-top: none;
-              border-right: none;
-              border-left: none;
-              background: none;
-              outline: none;
-              border-bottom: 1.5px solid #D6D6D6;
-              padding-bottom: 9px;
-              font-size: 13px;
-              width: 350px;
-            }
-          }
-        }
-      }
     }
   }
 }
